947d3da86d49216b8fd7a05bd7175a9fc253eb93
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-07-07 02:01:58 +0300

    CryptoCurrencyApp-Update

---
+++ cryptocurrency_app/src/components/HomePageSections/Statistics/Statistics.jsx
@@ -0,0 +1,7 @@
+const Statistics = () => {
+  return (
+    <div>Statistics</div>
+  )
+}
+
+export default Statistics
---
+++ cryptocurrency_app/src/components/HomePageSections/Statistics/StatisticsStyle.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const StatisticsContainer = styled.div `
+  background-color: #EAF5FB;
+`;
