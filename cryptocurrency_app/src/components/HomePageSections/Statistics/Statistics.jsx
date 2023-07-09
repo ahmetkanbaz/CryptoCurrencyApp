55ad80e460c8814381209e357aa5b551cd217db3
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-07-09 13:13:45 +0300

    CryptoCurrencyApp-Statistics-Section
    Anasayfada bulunan Statistic sectionı tamamlandı.

--- cryptocurrency_app/src/components/HomePageSections/Statistics/Statistics.jsx
+++ cryptocurrency_app/src/components/HomePageSections/Statistics/Statistics.jsx
@@ -1,7 +1,37 @@
+import SingleStatisticCard from "./SingleStatisticCard/SingleStatisticCard";
+import { StatisticsContainer } from "./StatisticsStyle";
 const Statistics = () => {
+  const statisticsDatas = [
+    {
+      image:
+        "https://demo.htmlcodex.com/2597/free-cryptocurrency-website-template/img/icon-9.png",
+      value: "123456",
+      subTitle: "Today Transactions",
+    },
+    {
+      image:
+        "https://demo.htmlcodex.com/2597/free-cryptocurrency-website-template/img/icon-10.png",
+      value: "123456",
+      subTitle: "Monthly Transactions",
+    },
+    {
+      image:
+        "https://demo.htmlcodex.com/2597/free-cryptocurrency-website-template/img/icon-2.png",
+      value: "123456",
+      subTitle: "Total Transactions",
+    },
+  ];
   return (
-    <div>Statistics</div>
-  )
-}
+    <div className="py-5">
+      <StatisticsContainer className="container py-5 rounded">
+        <div className="row">
+          {
+            statisticsDatas.map((statisticsData, index) => <SingleStatisticCard key={index} statisticsData = {statisticsData} />)
+          }
+        </div>
+      </StatisticsContainer>
+    </div>
+  );
+};
 
-export default Statistics
+export default Statistics;
--- cryptocurrency_app/src/views/Home/Home.jsx
+++ cryptocurrency_app/src/views/Home/Home.jsx
@@ -1,11 +1,13 @@
 import HeroBanner from "../../components/Banners/HomeBanners/HeroBanner/HeroBanner"
 import AboutCrypto from "../../components/HomePageSections/AboutCrypto/AboutCrypto"
+import Statistics from "../../components/HomePageSections/Statistics/Statistics"
 
 const Home = () => {
   return (
     <div>
       <HeroBanner />
       <AboutCrypto />
+      <Statistics />
     </div>
   )
 }
