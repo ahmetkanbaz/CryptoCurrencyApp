f8115ddbf483147a6b71523ce5028f53e612e82c
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-07-11 14:14:36 +0300

    CryptoCurrencyApp-AllCoins-Banner
    All Coins sayfasında bulunan banner tamamlandı. Background verildi ve yazıları yazıldı.

--- cryptocurrency_app/src/components/Banners/AllCoins/AllCoinsBannerStyle.js
+++ cryptocurrency_app/src/components/Banners/AllCoins/AllCoinsBannerStyle.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const AllCoinsBannerWrapper = styled.div`
-  background: linear-gradient(to bottom, rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),
+  background: linear-gradient(to bottom, rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)),
     url("https://images.unsplash.com/photo-1619942243797-0fd4e05f1702?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1293&q=80");
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+  height: 50vh;
 `;
